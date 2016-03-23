2c2ca63ec2bb5b6240ccfeac5fd353b612553536
savitri mhatre <savitri.mhatre@pridevel.com>
2016-03-23 10:57:42 +0000

    Added validation and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,20 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact =Contact.new(contact_params)
         
+        if @contact.save
+            flash[:success] = "Message Sent"
+           redirect_to new_contact_path
+       else
+           flash[:notice] = "Error occoured"
+           redirect_to new_contact_path
+        end
+    end
+    
+    private
+    def contact_params
+       params.require(:contact).permit(:name, :email,:comment) 
     end
     
 end
