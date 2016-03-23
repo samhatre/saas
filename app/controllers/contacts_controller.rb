eb7eea3919483c9d8e679738603484e07bc4e113
savitri mhatre <savitri.mhatre@pridevel.com>
2016-03-23 12:28:54 +0000

    Added mailer and view for mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,11 @@ class ContactsController < ApplicationController
         @contact =Contact.new(contact_params)
         
         if @contact.save
+            name = params[:contact][:name] #"contact"=>{"name"=>"test1","email"=>"","comments"=>""}
+            email = params[:contact][:email]
+            body = params[:contact][:comments]
+#calling ContactMailer file and contact_email method inside the mailer file and passing the arguments as we are passing to the action#            
+            ContactMailer.contact_email(name,email,body).deliver
             flash[:success] = "Message Sent"
            redirect_to new_contact_path
        else
