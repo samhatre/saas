01b49a5365f9ab133d213318233e6d0a97680e31
savitri mhatre <savitri.mhatre@pridevel.com>
2016-03-24 12:03:24 +0000

    Added plan model and db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324090517) do
+ActiveRecord::Schema.define(version: 20160324112042) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.text   "comments"
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
