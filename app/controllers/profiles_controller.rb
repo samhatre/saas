dd83e2bb31f23bd5d44886ed0d73e5004edae54c
savitri mhatre <savitri.mhatre@pridevel.com>
2016-03-28 10:36:42 +0000

    Added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,7 @@ class ProfilesController < ApplicationController
     end
   end
   
+  
     private
     def profile_params
        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
