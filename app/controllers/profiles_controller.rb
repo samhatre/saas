97dbc092abc24845d6d0be5168a1f6b48bbc3fc0
savitri mhatre <savitri.mhatre@pridevel.com>
2016-03-28 17:36:25 +0000

    added form partial and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,10 @@ class ProfilesController < ApplicationController
     end
   end
   
+  def edit
+      @user = User.find(params[:user_id])
+      @profile = @user.profile
+  end
   
     private
     def profile_params
