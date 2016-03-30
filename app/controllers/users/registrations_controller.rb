b4d4c3e78bd3c48a00a4bcbd13ed27e89291ad73
savitri mhatre <savitri.mhatre@pridevel.com>
2016-03-30 09:26:47 +0000

    Changes made to stripe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,14 +6,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
      super do |resource|
        if params[:plan]  # checking which plan is selected
           resource.plan_id = params[:plan] #storing that value in plan_id viz created in Users table i.e storing in db which plan the user is comming with
-       if resource.plan_id ==2
-           resource.save_with_payment
-       else
-           resource.save
-       end
+           if resource.plan_id == 2
+               resource.save_with_payment
+           else
+               resource.save
+           end
        end
       end   
    end
+   
   private
   def select_plan
       #checks whether plan is either 1 or 2 else redirect to home page
