d0d090cca23d07e9991192b55d1b3e219119c13c
savitri mhatre <savitri.mhatre@pridevel.com>
2016-03-30 18:34:55 +0000

    General Improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   def save_with_payment
     if valid?  #checks whether all the fields are fill properly
     #sending the information stored in customer variable to stripe 
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
