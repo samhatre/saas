36ce2dcc01ef77d40d271c12046da7323380052e
savitri mhatre <savitri.mhatre@pridevel.com>
2016-03-30 15:51:43 +0000

    added styles and corrected user show page and profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,13 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :only_current_user
+  
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
     @profile = Profile.new
   end
+  
   def create 
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
@@ -16,10 +18,12 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
+  
   def edit
     @user = User.find( params[:user_id] )
     @profile = @user.profile
   end
+  
   def update
     @user = User.find( params[:user_id] )
     @profile = @user.profile
@@ -30,10 +34,12 @@ class ProfilesController < ApplicationController
       render action: :edit
     end
   end
+  
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name,:avatar, :job_title, :phone_number, :contact_email, :description)
+     params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
+    
     def only_current_user
       @user = User.find( params[:user_id] )
       redirect_to(root_url) unless @user == current_user
